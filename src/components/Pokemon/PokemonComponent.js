00657c2dcf82dd7ad93419970f09d50a09c3785a
Igor Marques <soigormarques@gmail.com>
2022-12-17 16:47:44 -0300

    Tradução dos tipos

--- src/components/Pokemon/PokemonComponent.js
+++ src/components/Pokemon/PokemonComponent.js
@@ -3,17 +3,29 @@ import { Link } from "react-router-dom";
 
 const PokemonComponent = ({ name, img, params, types }) => {
   const typeHandler = () => {
-    if (types[1]) {
-      return (
-        <span className={styles.element}>
-          <span>{types[0].type.name}</span>
-          <span>{types[1].type.name}</span>
-        </span>
-      );
-    }
     return (
       <span className={styles.element}>
-        <span>{types[0].type.name}</span>
+        {types.map((type, index) => (
+          <span key={index}>
+            {type.type.name === "grass" ? "Grama" : ""}
+            {type.type.name === "poison" ? "Venenoso" : ""}
+            {type.type.name === "fire" ? "Fogo" : ""}
+            {type.type.name === "flying" ? "Voador" : ""}
+            {type.type.name === "water" ? "Água" : ""}
+            {type.type.name === "bug" ? "Inseto" : ""}
+            {type.type.name === "normal" ? "Normal" : ""}
+            {type.type.name === "electric" ? "Elétrico" : ""}
+            {type.type.name === "ground" ? "Terrestre" : ""}
+            {type.type.name === "fairy" ? "Fada" : ""}
+            {type.type.name === "fighting" ? "Lutador" : ""}
+            {type.type.name === "psychic" ? "Psíquico" : ""}
+            {type.type.name === "rock" ? "Pedra" : ""}
+            {type.type.name === "steel" ? "Aço" : ""}
+            {type.type.name === "ice" ? "Gelo" : ""}
+            {type.type.name === "ghost" ? "Fantasma" : ""}
+            {type.type.name === "ghost" ? "Fantasma" : ""}
+          </span>
+        ))}
       </span>
     );
   };
